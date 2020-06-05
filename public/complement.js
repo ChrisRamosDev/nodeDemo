4f02429700170065696ad8704e1b4aee71395341
Chris Ramos <Cramos7114@gmail.com>
2020-06-05 17:13:53 -0400

    first init of nodedemo proj

---
+++ public/complement.js
@@ -0,0 +1,14 @@
+document  
+.querySelector(".request-complement")
+.addEventListener('click', () => {
+  fetch("/complement")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      document.querySelector('.complement').innerText = data.complement;
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
